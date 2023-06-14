a8ba6c9ef3f26eef8d9c0f687d230bad331f23c7
BastleyNait <schirinosne@unsa.edu.pe>
2023-06-14 11:40:36 -0500

    estilos e implementacion de vista detalle

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('detalle/<str:title>', views.detalle, name='detalle'),
+    path('crear/', views.new, name='new' )
 ]
