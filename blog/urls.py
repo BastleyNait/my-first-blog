66105d382240b1db6d0fdf82f72f510f539de4d6
BastleyNait <schirinosne@unsa.edu.pe>
2023-06-14 13:05:11 -0500

    borrado

--- blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('detalle/<str:title>', views.detalle, name='detalle'),
-    path('crear/', views.new, name='new' )
-]
